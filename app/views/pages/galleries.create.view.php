6b43c16247ee783bb3691209740b04d992496e7e
menep <menep@protonmail.com>
2019-12-28 12:11:41 +0100

    add create view with form

---
+++ app/views/pages/galleries.create.view.php
@@ -0,0 +1,6 @@
+<h1>Create a new gallery</h1>
+
+<form action="galleries/create/submit" method="post">
+    <label>Title: <input type="text" name="title"></label>
+    <input type="submit" value="Create gallery">
+</form>
