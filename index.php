6bb175e35b892a6e455ead71f945501a12cdd801
menep <menep@protonmail.com>
2019-12-28 13:54:47 +0100

    add basic differentiation for request method routing in index.php

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php
 require 'app/core/utils.php';
 
-$view = parseUri() . ".view.php";
+$method = ($_SERVER['REQUEST_METHOD']);
 
-if (file_exists("app/views/pages/$view")) {
-    require "app/views/pages/$view";
-} else {
-    require 'app/views/pages/not-found.php';
+if ($method === 'GET') {
+    $view = parseUri() . ".view.php";
+
+    if (file_exists("app/views/pages/$view")) {
+        require "app/views/pages/$view";
+    } else {
+        require 'app/views/pages/not-found.php';
+    }
+} else if ($method === 'POST') {
+    $pdo = require 'app/core/pdo.php';
+
+    $stmt = $pdo->prepare('SELECT * FROM test');
+    $stmt->execute();
+    $response = $stmt->fetchAll(PDO::FETCH_CLASS);
+
+    foreach ($response as $row) {
+        echo '<pre>' . var_export($row->b, true) . '</pre>';
+    }
 }
