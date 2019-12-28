e67360dba255fae27ee17b1d69ec460dfbc9fd6c
menep <menep@protonmail.com>
2019-12-28 11:55:25 +0100

    update parseUri function to replace slashes with dots

--- app/core/utils.php
+++ app/core/utils.php
@@ -2,7 +2,7 @@
 
 function parseUri()
 {
-    $uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+    $uri = str_replace('/', '.', trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'));
 
     return (bool) $uri ? $uri : 'index';
 }
