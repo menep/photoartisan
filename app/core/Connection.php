e2da6f0da0727d028724927fadacb76f36be8076
menep <menep@protonmail.com>
2020-01-03 23:46:42 +0100

    add Connection class

---
+++ app/core/Connection.php
@@ -0,0 +1,20 @@
+<?php
+
+class Connection
+{
+    public static function make($config)
+    {
+        try {
+            $dsn = $config['connection'] . ';dbname=' . $config['dbname'];
+
+            return new PDO(
+                $dsn,
+                $config['username'],
+                $config['password'],
+                $config['options']
+            );
+        } catch (PDOException $error) {
+            die($error->getMessage());
+        }
+    }
+}
