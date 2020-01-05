354d2e86dbf9f28975d7a18f47f5c27ad8d395ef
menep <menep@protonmail.com>
2020-01-05 11:22:03 +0100

    require form partial in gallery create page

--- app/views/pages/galleries.create.view.php
+++ app/views/pages/galleries.create.view.php
@@ -1,11 +1,5 @@
 <h1>Create a new gallery</h1>
 
-<form action="/create/gallery" method="post">
-    <label>Title: <input type="text" name="title"></label>
-    <br>
-    <label>Description: <textarea name="description" rows="5" cols="33"></textarea></label>
-    <br>
-    <label>Author: <input type="text" name="author"></label>
-    <br>
-    <input type="submit" value="Create gallery">
-</form>
+<?php
+
+require 'app/views/partials/gallery.form.php';
