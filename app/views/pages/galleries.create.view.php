50c5f15eb9ef0906d7026224ee7221d6b96ac37b
menep <menep@protonmail.com>
2020-01-05 12:05:08 +0100

    move gallery creation form to galleries.view.php

--- app/views/pages/galleries.create.view.php
+++
@@ -1,8 +0,0 @@
-<h1>Create a new gallery</h1>
-
-<?php
-
-$formAction = 'gallery/create';
-$submitButtonLabel = "Create gallery";
-
-require 'app/views/partials/gallery.form.php';
--- app/views/pages/galleries.view.php
+++ app/views/pages/galleries.view.php
@@ -1,26 +1,34 @@
-<?php 
-
-require 'app/views/partials/header.php';
-
-$formAction = 'gallery/find';
-$submitButtonLabel = "Find gallery"
-
-?>
+<?php require 'app/views/partials/header.php'; ?>
 
 <main>
     <h1>Galleries</h1>
 
     <ul>
-        <li><a href="/galleries/create">Create new gallery</a></li>
         <li><a href="/galleries/list">View your galleries</a></li>
     </ul>
 
     <br>
 
+    <h2>Create a new gallery</h2>
+
+    <?php
+
+    $formAction = 'gallery/create';
+    $submitButtonLabel = "Create gallery";
+
+    require 'app/views/partials/gallery.form.php'; ?>
+
+    <br>
+
     <h2>Find a gallery</h2>
 
-    <?php require 'app/views/partials/gallery.form.php'; ?>
-    
+    <?php
+
+    $formAction = 'gallery/find';
+    $submitButtonLabel = "Find gallery";
+
+    require 'app/views/partials/gallery.form.php'; ?>
+
 </main>
 
 <?php
