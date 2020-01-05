43bb46065b19f447b1f97c50d8decde236dda2bd
menep <menep@protonmail.com>
2020-01-05 11:20:09 +0100

    update href in nav.php

--- app/views/partials/nav.php
+++ app/views/partials/nav.php
@@ -3,7 +3,7 @@
     <nav>
         <ul class="header__navlist">
             <li class="header__link"><a href="/">Home</a></li>
-            <li class="header__link"><a href="galleries">Galleries</a></li>
+            <li class="header__link"><a href="/galleries">Galleries</a></li>
             <li class="header__link"><a href="/about">About</a></li>
             <li class="header__link"><a href="/contact">Contact</a></li>
         </ul>
