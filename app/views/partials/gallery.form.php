54c36c515cdc9102b4b11fc80bcd6389662012ea
menep <menep@protonmail.com>
2020-01-05 11:20:35 +0100

    create galllery form partial

---
+++ app/views/partials/gallery.form.php
@@ -0,0 +1,9 @@
+<form action="/create/gallery" method="post">
+    <label>Title: <input type="text" name="title"></label>
+    <br>
+    <label>Description: <textarea name="description" rows="5" cols="33"></textarea></label>
+    <br>
+    <label>Author: <input type="text" name="author"></label>
+    <br>
+    <input type="submit" value="Create gallery">
+</form>
