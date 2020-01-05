580ac29d09ac16e9dec41151c01f6b01ddfbce33
menep <menep@protonmail.com>
2020-01-05 11:21:41 +0100

    remove static html galleries page

--- app/views/pages/galleries.view.php
+++ app/views/pages/galleries.view.php
@@ -1,7 +1,21 @@
+<?php require 'app/views/partials/header.php'; ?>
+
+<main>
+    <h1>Galleries</h1>
+
+    <ul>
+        <li><a href="/galleries/create">Create new gallery</a></li>
+        <li><a href="/galleries/list">View your galleries</a></li>
+    </ul>
+
+    <br>
+
+    <h2>Find a gallery</h2>
+
+    <?php require 'app/views/partials/gallery.form.php'; ?>
+    
+</main>
+
 <?php
-
-require 'app/views/partials/header.php';
-
-require 'public/static/galleries.html';
 
 require 'app/views/partials/footer.php';
