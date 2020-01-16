8cc7ef1eec91de5cf39531ef961de0f62495282e
menep <menep@protonmail.com>
2020-01-16 21:52:33 +0100

    add table and proper values in query result page

--- app/views/pages/galleries.results.php
+++ app/views/pages/galleries.results.php
@@ -6,15 +6,27 @@ require 'app/views/partials/header.php';
 
 <h1>Found galleries</h1>
 
-<?php
-foreach ($response as $row) {
-    foreach ($row as $key => $value) {
-        # code...
-        echo "<h2>$key : $value</h2>";
-    }
-}
+<table>
+    <thead>
+        <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Created</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($response as $key) : ?>
 
-?>
+            <tr>
+                <td><?= $key['title'] ?></td>
+                <td><?= $key['author']; ?></td>
+                <td><?= $key['created']; ?></td>
+            </tr>
+
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
 
 <?php
 require 'app/views/partials/footer.php';
