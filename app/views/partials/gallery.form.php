99da5396d6cae8b9b7cf731bbf0658cb15331459
menep <menep@protonmail.com>
2020-01-16 20:58:37 +0100

    add if block to gallery.form

--- app/views/partials/gallery.form.php
+++ app/views/partials/gallery.form.php
@@ -1,8 +1,12 @@
 <form action="<?= $formAction ?>" method="post">
     <label>Title: <input type="text" name="title"></label>
     <br>
-    <label>Description: <textarea name="description" rows="5" cols="33"></textarea></label>
-    <br>
+    
+    <?php if ($withDescription) : ?>
+        <label>Description: <textarea name="description" rows="5" cols="33"></textarea></label>
+        <br>
+    <?php endif; ?>
+
     <label>Author: <input type="text" name="author"></label>
     <br>
     <input type="submit" value="<?= $submitButtonLabel ?>">
