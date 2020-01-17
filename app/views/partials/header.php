9f1940a2da5cd3254c8c1a66dab54f7635c23ef2
menep <menep@protonmail.com>
2020-01-17 20:02:25 +0100

    update title

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PHP Practice Site</title>
+    <title>Photoartisan</title>
     <link rel="stylesheet" href="/public/assets/styles/base.css">
 </head>
 
