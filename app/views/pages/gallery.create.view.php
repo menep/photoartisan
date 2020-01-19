60cd8329a703951c7816c90a174a1b701c914770
menep <menep@protonmail.com>
2020-01-19 15:16:12 +0100

    remove galleries.view, add gallery.create

---
+++ app/views/pages/gallery.create.view.php
@@ -0,0 +1,13 @@
+<?php require 'app/views/partials/header.php'; ?>
+
+<h2>Create a new gallery</h2>
+
+<?php
+
+$formAction = 'galleries/create';
+$submitButtonLabel = "Create gallery";
+$withDescription = true;
+
+require 'app/views/partials/gallery.form.php';
+
+require 'app/views/partials/footer.php';
