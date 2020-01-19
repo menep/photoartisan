663b7557b9a111150e69f70894eec582acb0f13a
menep <menep@protonmail.com>
2020-01-19 16:16:44 +0100

    format timestamp to date

--- app/views/pages/gallery.results.view.php
+++ app/views/pages/gallery.results.view.php
@@ -6,27 +6,27 @@ require 'app/views/partials/header.php';
 
 <h1 class="title--1 text--center margin__bt--40"><?= (count($response) ?: 'No') . ' results found' ?></h1>
 
-<?php if(count($response)) : ?>
-<table class="table__query--results text--medium text--center">
-    <thead>
-        <tr>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Created</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($response as $key) : ?>
-
+<?php if (count($response)) : ?>
+    <table class="table__query--results text--medium text--center">
+        <thead>
             <tr>
-                <td><?= $key['title'] ?></td>
-                <td><?= $key['author']; ?></td>
-                <td><?= $key['created']; ?></td>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Created</th>
             </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($response as $key) : ?>
 
-        <?php endforeach; ?>
-    </tbody>
-</table>
+                <tr>
+                    <td><?= $key['title'] ?></td>
+                    <td><?= $key['author']; ?></td>
+                    <td><?= (new DateTime('@' . $key['created']))->format('d M Y, H:i'); ?></td>
+                </tr>
+
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 
 <?php endif ?>
 
