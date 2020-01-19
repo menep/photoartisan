0b537c4d96bd8e2a68066328d8a46076346569dc
menep <menep@protonmail.com>
2020-01-19 15:10:37 +0100

    update index file

--- app/views/pages/index.view.php
+++ app/views/pages/index.view.php
@@ -4,4 +4,15 @@
 
 <h2 class="title--2 text--center"> A simple app to create and view photo galleries </h2>
 
+<section class="text--center margin__top--40">
+    <ul class="text--medium">
+        <li>
+            <a href="gallery/create">Create a gallery</a>
+        </li>
+        <li class="margin__top--10">
+            <a href="gallery/find">Find a gallery</a>
+        </li>
+    </ul>
+</section>
+
 <?php require 'app/views/partials/footer.php';
