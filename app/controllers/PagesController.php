4e5bfaecb2eda2915a4a02c042aeee97cfed5daa
menep <menep@protonmail.com>
2020-01-21 20:39:11 +0100

    restructure pagesController

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -2,8 +2,37 @@
 
 class PagesController
 {
-    public function getPage($page)
+    public function home()
     {
-        require "app/views/pages/$page.php";
+        require "app/views/pages/index.php";
+    }
+
+    public function notFound()
+    {
+        require "app/views/pages/not-found.php";
+    }
+
+    public function gallery($action, $method)
+    {
+        $routes = [
+            'GET' => [
+                'find',
+                'create'
+            ],
+            'POST' => [
+                'find',
+                'create'
+            ]
+        ];
+
+        if (array_search($action, $routes[$method]) !== false) {
+            if ($method === 'GET') {
+                require "app/views/pages/gallery.$action.php";
+            } else if ($method === 'POST') {
+                (new GalleryController())->$action($_POST);
+            }
+        } else {
+            (new PagesController())->notFound();
+        }
     }
 }
