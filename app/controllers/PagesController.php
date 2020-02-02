e517bda05b06401d4d1b6ee1a80a5acf45c267ad
menep <menep@protonmail.com>
2020-02-02 17:45:07 +0100

    add viewGallery method to PagesController

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -21,5 +21,14 @@ class PagesController
     public function createGallery()
     {
         require "app/views/pages/gallery.create.php";
-    }
+	}
+	
+	public function viewGallery()
+	{
+		$params = Request::getParams();
+
+        $response = (new Query(DatabaseConnection::make()))->findGalleryById($params['id']);
+
+        require "app/views/pages/gallery.view.php";
+	}
 }
