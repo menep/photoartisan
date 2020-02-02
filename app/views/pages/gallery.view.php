367049a947b1b6826a4fa026b1a35c315c281a38
menep <menep@protonmail.com>
2020-02-02 17:45:48 +0100

    add gallery.view.php view

---
+++ app/views/pages/gallery.view.php
@@ -0,0 +1,17 @@
+<?php require 'app/views/partials/header.php'; ?>
+
+<?php if (!$response) : ?>
+
+<h1 class="title--1 text--center margin__bt--20">Oops!</h1>
+<h2 class="title--2 text--center">The gallery you were looking for could not be retrieved...</h2>
+
+<?php else : ?>
+
+<h1 class="title--1 text--center margin__bt--40"><?= $response['title'] ?> by <?= $response['author'] ?>
+</h1>
+<h2 class="title--2 text--center margin__bt--40"><?= $response['description'] ?>
+</h2>
+
+<?php endif; ?>
+
+<?php require 'app/views/partials/footer.php';
