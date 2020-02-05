dac86028e4225f5960b83310665473878877862b
menep <menep@protonmail.com>
2020-02-05 20:22:20 +0100

    add form to upload image

--- app/views/pages/gallery.view.php
+++ app/views/pages/gallery.view.php
@@ -12,6 +12,13 @@
 <h2 class="title--2 text--center margin__bt--40"><?= $response['description'] ?>
 </h2>
 
+<section>
+	<form action="upload" method="post" class="form__wrapper text--medium">
+		<label>Select a new image: <input type="file" name="file" accept="image/*"></label>
+		<input type="submit" value="Upload image">
+	</form>
+</section>
+
 <?php endif; ?>
 
 <?php require 'app/views/partials/footer.php';
